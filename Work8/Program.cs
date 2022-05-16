479b770d24c835d6854c4de6778662fb9040cae1
Nurgaleeva Elmira <Elmira8013@yandex.ru>
2022-05-16 21:04:04 +0300

    Задача Work8

---
+++ Work8/Program.cs
@@ -0,0 +1,14 @@
+﻿//Задача 17  Напишите программу, которая принимает на вход
+//координаты точки (X и Y), причём X ≠ 0 и Y ≠ 0 и выдаёт
+//номер четверти плоскости, в которой находится эта
+//точка
+
+Console.WriteLine("введите координату X : ");
+int x = int.Parse(Console.ReadLine());
+Console.WriteLine("введите координату X : ");
+int y = int.Parse(Console.ReadLine());
+if (x > 0 && y > 0)
+{
+    Console.WriteLine("4");
+}
+else if (x > 0 && y > 0)
