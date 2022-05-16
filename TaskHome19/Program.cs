afe9356d04702d10b553f68a84ed727840f9c57e
Nurgaleeva Elmira <Elmira8013@yandex.ru>
2022-05-16 22:48:02 +0300

    Решена задача№ 19 Домашнее задание Семинар3

--- TaskHome19/Program.cs
+++ TaskHome19/Program.cs
@@ -5,5 +5,36 @@
 //23432 -> да
 //12821 -> да
 
+Console.WriteLine("Введите пятизначное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
 
+int temp1 = num;
 
+int firstDigit = temp1 / 10000;
+temp1 = temp1 % 10000;
+
+int secondDigit = temp1 / 1000;
+temp1 = temp1 % 1000;
+
+temp1 = temp1 % 100;
+
+int fourthDigit = temp1 / 10;
+temp1 = temp1 % 10;
+
+int fifthDigit = temp1;
+
+if (firstDigit == fifthDigit)
+{
+    if (secondDigit == fourthDigit)
+    {
+        Console.Write("Это палиндром!");
+    }
+    else
+    {
+        Console.Write("Это не палиндром!");
+    }
+}
+else
+{
+    Console.Write("Это не палиндром!");
+}
