275236728702a802c224bbc75a27b4b6a298426b
Nurgaleeva Elmira <Elmira8013@yandex.ru>
2022-05-18 13:03:38 +0300

    Добавлены комментарии к задаче 18

--- Work9/Program.cs
+++ Work9/Program.cs
@@ -4,20 +4,26 @@
 
 Console.WriteLine("введите номер четверти : ");
 int nunber = int.Parse(Console.ReadLine());
+// можно использовать var вместо int  Это универсальная переменная! Здесь результат тотже
+//так как здесь ReadLine то это тип данных Строковой а у нас число, нельзя сравнить строку с числом, 
+//соотвественно кавычки оставляем! "1" "2" Например 
+//Console.WriteLine("введите номер четверти : ");
+//int nunber = Console.ReadLine();   без конвертации строки в int  ! тогда все цифры указать в ковычках
+//Если есть конвертация строки в int  : int nunber = int.Parse(Console.ReadLine())   - то ковычки у числе не нужны !
 
-if (nunber ==1)
+if (nunber == 1)
 {
     Console.WriteLine("x > 0 и y > 0");
 }
-else if (nunber ==2)
+else if (nunber == 2) 
 {
     Console.WriteLine("x < 0 и y > 0");
 }
-else if (nunber ==3)
+else if (nunber == 3)
 {
     Console.WriteLine("x < 0 и y < 0");
 }
-else if (nunber ==4)
+else if (nunber == 4)
 {
     Console.WriteLine("x > 0 и y < 0");
 }
