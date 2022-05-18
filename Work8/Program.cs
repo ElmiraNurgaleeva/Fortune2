f73007c89d93eb61d379cd31a9ec7ceb250045f0
Nurgaleeva Elmira <Elmira8013@yandex.ru>
2022-05-18 12:08:59 +0300

    Решена задача 17 на Семинаре3

--- Work8/Program.cs
+++ Work8/Program.cs
@@ -5,10 +5,21 @@
 
 Console.WriteLine("введите координату X : ");
 int x = int.Parse(Console.ReadLine());
-Console.WriteLine("введите координату X : ");
+Console.WriteLine("введите координату Y : ");
 int y = int.Parse(Console.ReadLine());
 if (x > 0 && y > 0)
 {
-    Console.WriteLine("4");
+    Console.WriteLine("Диапазон находится в 1 четверти");
 }
-else if (x > 0 && y > 0)
+else if (x < 0 && y > 0)
+{
+    Console.WriteLine("Диапазон находится в 2 четверти");
+}
+else if (x < 0 && y < 0)
+{
+    Console.WriteLine("Диапазон находится в 3 четверти");
+}
+else if (x > 0 && y < 0)
+{
+    Console.WriteLine("Диапазон находится в 4 четверти");
+}
