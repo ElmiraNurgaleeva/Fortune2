8b6e2baa6faea1b11016e1fa96c5cbaf051401d8
Nurgaleeva Elmira <Elmira8013@yandex.ru>
2022-05-24 14:13:05 +0300

    TaskHome38

--- TaskHome34/Program.cs
+++ TaskHome34/Program.cs
@@ -5,3 +5,43 @@
 // [345, 897, 568, 234] -> 2
 
 
+void FillArray(int[] collection)    //Метод который сами опишем 
+                                    //+ Дадим наименование Методу
+                                    //Void метод ничего не возвращает 
+                                    //Return не используем в коде 
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(100, 1000);
+        Console.WriteLine("Массив " + collection[index]);
+        //положить новое случайное число из диапазона от 1 до 10
+        index++;
+    }
+}
+
+void PrintArray(int[] col)   //Метод Войд который будет печатать массив
+{
+    int count = col.Length;
+    int position = 0;
+
+
+while (position < count)
+    {
+       
+        if (col[position] % 2 == 0)
+        Console.WriteLine("Четные числа массива " + col[position]);
+        position++;
+    }
+}
+
+int[] array = new int[10]; //в массиве будет 10 элементов 
+                           //new int [10] дословно создай новый массив в котором будет 10 элементов
+                       
+FillArray(array);
+PrintArray(array);
+
+
+
+
